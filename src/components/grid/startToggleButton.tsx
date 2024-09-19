3c04385f63a695f0f237b78140ce27d3fb142727
BoKyeong Shin <helenshin97@gmail.com>
2024-09-20 00:51:21 +0900

    feat: add useMutate for updating starred value

--- src/components/grid/startToggleButton.tsx
+++ src/components/grid/startToggleButton.tsx
@@ -1,7 +1,9 @@
-import { IconButton } from "@mui/material";
-import { MouseEvent } from "react";
-import StarOutlineIcon from "@mui/icons-material/StarOutline";
-import StarIcon from "@mui/icons-material/Star";
+import { IconButton } from '@mui/material';
+import { MouseEvent } from 'react';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import StarIcon from '@mui/icons-material/Star';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateStar } from '../../apis';
 
 interface StartToggleButtonProp {
   isStarred: boolean;
@@ -12,19 +14,30 @@ export const StartToggleButton: React.FC<StartToggleButtonProp> = ({
   locationId,
   isStarred,
 }) => {
-  const handleToggle = (e: MouseEvent) => {
+  const queryClient = useQueryClient();
+  const { mutateAsync } = useMutation({
+    mutationFn: () => updateStar(locationId, !isStarred),
+  });
+  const handleToggle = async (e: MouseEvent) => {
     e.stopPropagation();
     // update star > put starred_location_id
+    try {
+      await mutateAsync();
+      queryClient.invalidateQueries();
+    } catch (error) {
+      // TODO: alert
+      console.log('error');
+    }
   };
 
   return (
     <IconButton
       aria-label="star"
-      sx={{ color: isStarred ? "" : "8E8E8E" }}
+      sx={{ color: isStarred ? '' : '8E8E8E' }}
       onClick={handleToggle}
     >
-      {!isStarred && <StarOutlineIcon sx={{ color: "#8E8E8E" }} />}
-      {isStarred && <StarIcon sx={{ color: "#F7B500" }} />}
+      {!isStarred && <StarOutlineIcon sx={{ color: '#8E8E8E' }} />}
+      {isStarred && <StarIcon sx={{ color: '#F7B500' }} />}
     </IconButton>
   );
 };
